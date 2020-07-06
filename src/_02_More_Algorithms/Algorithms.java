ff9ad9e1cffb9ae434e6c6854210a1a95bec5a16
root <root@d428aa78611f>
2020-07-06 23:27:23 +0000

    Finish Module2

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,6 +1,9 @@
 package _02_More_Algorithms;
 
+import java.util.Arrays;
 import java.util.List;
+
+import _00_Sorting_Algorithms.SortingVisualizer;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
@@ -32,8 +35,59 @@ public class Algorithms {
 	public static String findLongestWord(List<String> words) {
 		String longestWord = "";
 		for (int i = 0; i < words.size(); i++) {
-			if()
+			if(longestWord.length()<words.get(i).length()) {
+				longestWord = words.get(i);
+			}
 		}
 		return longestWord;
+	}
+	public static boolean containsSOS(List<String> message) {
+		if(message.contains("... --- ...")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static List<Double> sortScores(List<Double> list) {
+		Double[] array = (Double[]) list.toArray();
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length-1; j++) {
+				if(array[j+1]<array[j]) {
+					double w = array[j+1];
+					array[j+1] = array[j];
+					array[j] = w;
+				}
+			}
+		}
+		list = (List<Double>) Arrays.asList(array);
+		return list;
+	}
+	public static List<String> sortDNA(List<String> list) {
+		String[] array = (String[]) list.toArray();
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length-1; j++) {
+				if(array[j+1].length()<array[j].length()) {
+					String w = array[j+1];
+					array[j+1] = array[j];
+					array[j] = w;
+				}
+			}
+		}
+		list = (List<String>) Arrays.asList(array);
+		return list;
+	}
+	public static List<String> sortWords(List<String> list){
+		String[] array = (String[]) list.toArray();
+		for(int j = 0; j<array.length; j++) {
+			for (int i = 0; i < array.length-1; i++) {
+				if(array[i].compareTo(array[i+1])>0) {
+					String w = array[i+1];
+					array[i+1] = array[i];
+					array[i] = w;
+				}
+			}
+		}
+		list = (List<String>) Arrays.asList(array);
+		return list;
 	}
 }
